9771ac087914d966887a09d3eb8cfaf78a1a7a31
Erik M <failfan13gta5@gmail.com>
2022-10-09 16:04:54 +0200

    add Assignment studentData

---
+++ Arch2/Week5/studentNRAssignment/file.py
@@ -0,0 +1,71 @@
+
+import os
+import sys
+
+valid_lines = []
+corrupt_lines = []
+
+def validate_data(line):
+    invalid = []
+    corrupt = False
+    data = line.split(',')
+    studentNr = validate_studentNr(data[0])
+    name = validate_name(data[1], data[2])
+    date = validate_date(data[3])
+    program = validate_program(data[4])
+    func = [studentNr, name, date , program]
+    for var in func:
+        if var != None:
+            invalid.append(var)
+            corrupt = True
+    if corrupt == False:
+        valid_lines.append(line)
+    else:
+        text = ', => INVALID DATA: '
+        corrupt_lines.append(line+text+str(invalid))
+
+def validate_name(first, last):
+    name = [first, last]
+    for name in name:
+        if len(name) < 1:
+            return name
+        elif name.isalpha() != True:
+            return name
+
+def validate_studentNr(num):
+    if len(num) != 7:
+        return num
+    elif num[:2] not in ('08', '09'):
+        return num
+
+def validate_date(date):
+    try:
+        datum = date.split('-')
+        if int(datum[0]) not in range(1960, 2005):
+            return date
+        if int(datum[1]) not in range(1, 13):
+            return date
+        if int(datum[2]) not in range(1, 32):
+            return date
+    except:
+        return date
+
+def validate_program(val):
+    if val not in ('INF', 'TINF', 'CMD', 'AI'):
+        return val
+
+def main(csv_file):
+    with open(os.path.join(sys.path[0], csv_file), newline='') as csv_file:
+        next(csv_file)
+
+        for line in csv_file:
+            validate_data(line.strip())
+
+    print('### VALID LINES ###')
+    print("\n".join(valid_lines))
+    print('### CORRUPT LINES ###')
+    print("\n".join(corrupt_lines))
+
+
+if __name__ == "__main__":    
+    main('students.csv')
