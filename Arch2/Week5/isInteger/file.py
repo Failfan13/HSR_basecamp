c858cfcb52016dd576d09f489718c64cc34a4e6b
Erik M <failfan13gta5@gmail.com>
2022-10-09 19:46:57 +0200

    first commit

---
+++ Arch2/Week5/isInteger/file.py
@@ -0,0 +1,29 @@
+
+
+def is_integer(str):
+    for i in str:
+        if i.isdigit() == False:
+            return False
+    if len(str) < 1:
+        return False
+    return True
+
+
+def remove_non_integer(str):
+    num = ''
+    for i in str:
+        if ord(i) in range(48, 58) or ord(i) == 45:
+            num+=i
+    if num[0] == '-' and num[1] == '0':
+        num = num.replace('0', '')
+    return num
+
+
+if __name__ == '__main__':
+    inp = input('enter a number')
+    if is_integer(inp.replace(' ','')) is not True:
+        print('invalid')
+    else:
+        print('valid')
+
+    print(remove_non_integer(inp))
