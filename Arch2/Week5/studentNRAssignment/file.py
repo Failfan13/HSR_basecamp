5264ec663f0a9df17e3fdbd0cf1e309184fc6083
Erik M <failfan13gta5@gmail.com>
2022-10-09 16:29:28 +0200

    changes Assignment

--- Arch2/Week5/studentNRAssignment/file.py
+++ Arch2/Week5/studentNRAssignment/file.py
@@ -5,6 +5,7 @@ import sys
 valid_lines = []
 corrupt_lines = []
 
+
 def validate_data(line):
     invalid = []
     corrupt = False
@@ -13,30 +14,37 @@ def validate_data(line):
     name = validate_name(data[1], data[2])
     date = validate_date(data[3])
     program = validate_program(data[4])
-    func = [studentNr, name, date , program]
+    func = [studentNr, name, date, program]
     for var in func:
-        if var != None:
+        if var is not None:
             invalid.append(var)
             corrupt = True
-    if corrupt == False:
+    if corrupt is not True:
         valid_lines.append(line)
     else:
-        text = ', => INVALID DATA: '
-        corrupt_lines.append(line+text+str(invalid))
+        text = ' => INVALID DATA: '
+        corrupt_lines.append(line + text + str(invalid))
+
 
 def validate_name(first, last):
     name = [first, last]
     for name in name:
         if len(name) < 1:
             return name
-        elif name.isalpha() != True:
-            return name
+        else:
+            for char in name:
+                if (
+                    char.isalpha() is not True
+                        and char.isspace() is not True):
+                    return name
+
 
 def validate_studentNr(num):
     if len(num) != 7:
         return num
     elif num[:2] not in ('08', '09'):
         return num
+
 
 def validate_date(date):
     try:
@@ -47,12 +55,14 @@ def validate_date(date):
             return date
         if int(datum[2]) not in range(1, 32):
             return date
-    except:
+    except ValueError:
         return date
+
 
 def validate_program(val):
     if val not in ('INF', 'TINF', 'CMD', 'AI'):
         return val
+
 
 def main(csv_file):
     with open(os.path.join(sys.path[0], csv_file), newline='') as csv_file:
@@ -67,5 +77,5 @@ def main(csv_file):
     print("\n".join(corrupt_lines))
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     main('students.csv')
