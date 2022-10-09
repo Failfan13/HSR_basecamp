7cf0c3f651572889b65b7c6e3364a7da149d310d
Erik M <failfan13gta5@gmail.com>
2022-10-09 19:51:01 +0200

    changes Isinteger

--- Arch2/Week5/isInteger/file.py
+++ Arch2/Week5/isInteger/file.py
@@ -2,7 +2,7 @@
 
 def is_integer(str):
     for i in str:
-        if i.isdigit() == False:
+        if i.isdigit() is False:
             return False
     if len(str) < 1:
         return False
@@ -13,7 +13,7 @@ def remove_non_integer(str):
     num = ''
     for i in str:
         if ord(i) in range(48, 58) or ord(i) == 45:
-            num+=i
+            num += i
     if num[0] == '-' and num[1] == '0':
         num = num.replace('0', '')
     return num
@@ -21,7 +21,7 @@ def remove_non_integer(str):
 
 if __name__ == '__main__':
     inp = input('enter a number')
-    if is_integer(inp.replace(' ','')) is not True:
+    if is_integer(inp.replace(' ', '')) is not True:
         print('invalid')
     else:
         print('valid')
