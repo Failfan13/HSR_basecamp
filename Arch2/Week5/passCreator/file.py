2faa45b70fabb1b852da1ef0522af20694f4cb33
Erik M <failfan13gta5@gmail.com>
2022-10-09 20:03:25 +0200

    changes passCreator

--- Arch2/Week5/passCreator/file.py
+++ Arch2/Week5/passCreator/file.py
@@ -4,7 +4,7 @@ from random import randint
 def generate_random_password():
     randLen = randint(7, 10)
     password = []
-    for _ in range(0, randLen+1):
+    for _ in range(0, randLen + 1):
         char = chr(randint(33, 127))
         password.append(char)
     return ''.join(password)
