5edccb74dce08aedd7d43d688773059fc264e91e
Erik M <failfan13gta5@gmail.com>
2022-10-15 00:59:43 +0200

    first commit

---
+++ Arch2/Week6/validPassword/file.py
@@ -0,0 +1,19 @@
+def pass_check(inp):
+    valid = False
+    while valid is False:
+        print(inp)
+        valid = True
+    return
+
+
+def num_check(digit):
+    print(digit)
+
+
+def letter_check(char):
+    print(char)
+
+
+if __name__ == '__main__':
+    inp = input('your password?')
+    pass_check(inp)
