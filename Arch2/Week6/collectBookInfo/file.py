753db6e16a4afdc63196b664f83784bbc1216548
Erik M <failfan13gta5@gmail.com>
2022-10-16 13:22:00 +0200

    changes collectBookInfo

--- Arch2/Week6/collectBookInfo/file.py
+++ Arch2/Week6/collectBookInfo/file.py
@@ -6,10 +6,11 @@ def choices():
     inp = input('A = add book, S = Search book, E = Exit').lower()
     if inp in (options[0], options[3]):
         enter = input('please enter: title, author, publisher, date')
-        return print(add_book(enter))
+        add_book(enter)
+        return
     elif inp in (options[1], options[4]):
         enter = input('please search term')
-        return print(search_book(book_list, enter))
+        search_book(book_list, enter)
     elif inp in (options[2], options[5]):
         print('exit')
         return False
@@ -19,7 +20,7 @@ def choices():
 
 
 def add_book(book):
-    book = book.replace(' ', '').split(',')
+    book = book.split(',')
     if search_book(book_list, book[0]) is True:
         return 'invalid'
     book_list.append(
@@ -27,13 +28,18 @@ def add_book(book):
          'author': book[1],
          'publisher': book[2],
          'pub_date': book[3]})
+    for books in book_list:
+        print(books)
+    return book_list
 
 
 def search_book(books, term):
     for dicts in books:
         if term in dicts.values():
-            return True
-    return False
+            print('yes')
+            return print('True')
+    print('no')
+    return print('False')
 
 
 if __name__ == "__main__":
