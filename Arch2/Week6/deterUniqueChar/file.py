50390acd974248144e3a663442fa0b3559291813
Erik M <failfan13gta5@gmail.com>
2022-10-16 12:32:32 +0200

    changes deterUniqueChar

--- Arch2/Week6/deterUniqueChar/file.py
+++ Arch2/Week6/deterUniqueChar/file.py
@@ -1,36 +1,40 @@
 # inputs
-dictionary = {chr(key): 0 for key in range(32, 127)}
+dictionary = dict({chr(key): 0 for key in range(32, 127)})
 
-set = {chr(key) for key in range(32, 127)}
+set = set({chr(key) for key in range(32, 127)})
 
 # processor
 
 
-def unique_chars_dict(inp, dict):
-    if inp in dict.keys():
-        dict[inp] = 1
-
-
-def unique_chars_set(inp, set):
-    if inp in set:
-        set.remove(inp)
-
-
-def total_unique(dict):
-    unique_dict = 0
-    for value in dict.values():
+def unique_chars_dict(inp):
+    for char in inp:
+        if char in dictionary.keys():
+            dictionary[char] = 1
+        unique_dict = 0
+    for value in dictionary.values():
         if value != 0:
             unique_dict += 1
-
     print(unique_dict)
+    return unique_dict
+
+
+def unique_chars_set(inp):
+    for char in inp:
+        if char in set:
+            set.remove(char)
     print(95 - len(set))
+    return 95 - len(set)
+
+
+def total_unique(dict, set):
+    print(lendict)
+    print(lenset)
 
 
 # printer
 if __name__ == "__main__":
     inp = input('choose a word')
-    for char in inp:
-        unique_chars_dict(char, dictionary)
-        unique_chars_set(char, set)
+    lendict = unique_chars_dict(inp)
+    lenset = unique_chars_set(inp)
 
-total_unique(dictionary)
+    total_unique(lendict, lenset)
