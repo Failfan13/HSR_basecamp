0618b5a998883768f9db12e87069f048ced0f60c
Erik M <failfan13gta5@gmail.com>
2022-11-06 03:45:59 +0100

    changes challengeInterface_Mobile

--- Arch2/Week8-Challenge/interface.js
+++ Arch2/Week8-Challenge/interface.js
@@ -1,3 +1,36 @@
-import dataArrs from './modula/jsonRaspData.js'
+import dataArrs from './modula/jsonRaspData.js';
+import {createChart, createChartData} from './modula/chart.js';
 
-console.log(dataArrs())
+const show = document.querySelectorAll('.show')
+
+const dataVisuals = (data) => {
+    const dataContainer = document.querySelector('.info');
+    const labels = ['mean', 'min', 'max', 'devi']
+    const dataBox = document.createElement('article')
+    for (let i = 0; i <= 4; i++){
+        let element = document.createElement('p')
+        element.className = 'data';
+        let codex = 0;
+        if (labels[i] == labels[1]) {
+            codex = Math.min.apply(Math, data[1][0]['points']).toFixed(2)
+        }
+        if (labels[i] == labels[2]) {
+            codex = Math.max.apply(Math, data[1][0]['points']).toFixed(2)
+        }
+        element.innerHTML = labels[i]+': '+codex
+        dataBox.appendChild(element)
+    }
+    dataContainer.insertBefore(dataBox, dataContainer.childNodes[0])
+    document.createElement('p');
+    console.log(data[1][0]['points'])
+}
+
+show.forEach(i => {
+    let chart = document.createElement('canvas');
+    let title = i.className.split(' ')[1]
+    chart.id = title+' '+"myChart";
+    i.appendChild(chart)
+    let data = createChartData(dataArrs(), title)
+    dataVisuals(data)
+    createChart(i.children, data[0], data[1])
+})
---
+++ Arch2/Week8-Challenge/modula/chart.js
@@ -0,0 +1,69 @@
+export function createChart(canvas, labels, dataObject) {
+    const dataset = []
+    dataObject.forEach(key => {dataset.push({
+        label: key['title'],
+        data: key['points'],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        pointStyle: 'crossRot',
+        pointRadius: 5,
+        borderWidth: 2,
+        tension: 0.1,
+        fill: false,
+    })})
+    new Chart(canvas, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: dataset
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
+                },
+            }
+        }
+    });
+}
+
+export function createChartData(servData, title){
+    const data = []
+    const servTime = []
+    for (let i in servData) {
+        if (i == 'time') {
+            servData[i].sort();
+            servData[i].forEach(i => {
+                const date = new Date(i*1000);
+                servTime.push(date.getHours()+'.'+date.getMinutes())
+            })
+        } 
+        if (i == title) {
+            let title = '';
+            if (i == 'temp') {
+                title = 'Temperature over time';
+            } else if (i == 'humid'){
+                title = 'Humidity over time'
+            } else {
+                title = 'Pressure over time'
+            }
+            data.push({'title': title, 'points' : servData[i]})
+            break
+        }
+    }
+    return [servTime, data]
+}
