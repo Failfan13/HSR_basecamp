5b83e460efb2c95bb71cfb4eee238098dcceea8a
Erik M <failfan13gta5@gmail.com>
2022-11-10 19:36:04 +0100

    changes productPurchase

--- Arch2/Week9/productPurchase/file.py
+++ Arch2/Week9/productPurchase/file.py
@@ -4,20 +4,15 @@ class Product:
         self.amount = amount
         self.price = price
 
-    def get_price(self):
+    def get_price(self, amount):
         total = 0
-        if self.amount in range(10, 100):
-            total = (self.amount*self.price)/100*90
-        elif self.amount > 100:
-            total = (self.amount*self.price)/100*80
+        if amount in range(10, 99):
+            total = (amount * self.price) / 100 * 90
+        elif amount > 99:
+            total = (amount * self.price) / 100 * 80
         else:
-            total = (self.amount*self.price)
-        return total
+            total = (amount * self.price)
+        print(round(total))
 
-    def make_purchase(self):
-        ...
-
-
-if __name__ == "__main__":
-    prod = Product('milk', 12)
-    print(prod.get_price())
+    def make_purchase(self, amount):
+        self.amount -= amount
