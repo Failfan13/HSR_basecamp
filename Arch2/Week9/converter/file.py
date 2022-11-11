07cf379a9320723f32202bd2ddb3be3b45d911d1
Erik M <failfan13gta5@gmail.com>
2022-11-11 18:41:30 +0100

    changes converter

--- Arch2/Week9/converter/file.py
+++ Arch2/Week9/converter/file.py
@@ -1,38 +1,38 @@
 class Converter:
     def __init__(self, length, numeric):
         self.length = length
-        self.numeric = numeric
         self.numerics = {
-            'inches': self.length/25.4,
-            'feet': self.length/304.8,
-            'yards': self.length/914.4,
-            'miles': self.length/1609344,
-            'kilometers': self.length/1000000,
-            'meters': self.length/1000,
-            'centimeters': self.length/10,
+            'inches': self.length * 25.4,
+            'feet': self.length * 304.8,
+            'yards': self.length * 914.4,
+            'miles': self.length * 1609344,
+            'kilometers': self.length * 1000000,
+            'meters': self.length * 1000,
+            'centimeters': self.length * 10,
             'millimeters': self.length,
         }
+        self.numeric = self.numerics.get(numeric)
 
     def inches(self):
-        return self.numerics[self.numeric]*25.4
-    
+        return self.numeric / 25.4
+
     def feet(self):
-        return self.numerics[self.numeric]*304.8
+        return self.numeric / 304.8
 
     def yards(self):
-        return self.numerics[self.numeric]*914.4
+        return self.numeric / 914.4
 
     def miles(self):
-        return self.numerics[self.numeric]*1609344
+        return self.numeric / 1609344
 
     def kilometers(self):
-        return self.numerics[self.numeric]*1000000
+        return self.numeric / 1000000
 
     def meters(self):
-        return self.numerics[self.numeric]*1000
+        return self.numeric / 1000
 
     def centimeters(self):
-        return self.numerics[self.numeric]*10
+        return self.numeric / 10
 
     def millimeters(self):
-        return self.numerics[self.numeric]
+        return self.numeric
