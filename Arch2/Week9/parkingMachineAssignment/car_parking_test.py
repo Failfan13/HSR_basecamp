fef239822cfa79138ddde216dd6867e2619041e8
Erik M <failfan13gta5@gmail.com>
2022-11-12 18:13:04 +0100

    changes parkingMachine

--- Arch2/Week9/parkingMachineAssignment/car_parking_test.py
+++ Arch2/Week9/parkingMachineAssignment/car_parking_test.py
@@ -1,16 +1,11 @@
-from file import *
+from carparking import *
 from datetime import datetime, timedelta
 
-'''
-PyTest template file. Use asserts to test your class code.
-Don't forget to put this file in the same location as your carparkingmachine.py
-
-Hint: use the datetime module (datetime and timedelta objects) to test with a check-in time in the past.
-Example: my_datetime = datetime.now() - timedelta(hours=2, minutes=10)
-This statement creates a datetime object 2 hours and 10 minutes in the past.
-'''
-# object
 cmp = CarParkingMachine(capacity=2)
+
+# object
+def test_carparkingmachine():
+    assert cmp != None
 
 # Test check_in & parked_cars
 def test_check_in_capacity():
@@ -18,26 +13,26 @@ def test_check_in_capacity():
     cmp.check_in('21-TIV-8')
     assert len(cmp.parked_cars) == 2
     assert cmp.check_in('22-TId-9') == False
-    cmp.parked_cars = {}
-
+    cmp.check_out('BB-49-JF')
+    cmp.check_out('21-TIV-8')
 
 # Test parking fee
 def test_parking_fee():
-    cmp.check_in('GH-56-31', (datetime.now() - timedelta(hours=5, minutes=00)).strftime("%d:%H:%M"))
-    cmp.check_in('jkc-4-gi', (datetime.now() - timedelta(hours=30, minutes=40)).strftime("%d:%H:%M"))
+    cmp.check_in('GH-56-31', (datetime.now() - timedelta(hours=5, minutes=00)))
+    cmp.check_in('jkc-4-gi', (datetime.now() - timedelta(hours=30, minutes=40)))
     assert cmp.get_parking_fee('GH-56-31') == 15.0
     assert cmp.get_parking_fee('jkc-4-gi') == 60.0
-    cmp.parked_cars = {}
+    cmp.check_out('GH-56-31')
+    cmp.check_out('jkc-4-gi')
 
 # Test check-out
 def test_check_out():
-    cmp.check_in('14-HGD-6', (datetime.now() - timedelta(hours=5, minutes=00)).strftime("%d:%H:%M"))
+    cmp.check_in('14-HGD-6', (datetime.now() - timedelta(hours=5, minutes=00)))
     cmp.get_parking_fee('14-HGD-6')
-    assert cmp.parked_cars.get('14-HGD-6') == None 
-    cmp.parked_cars = {}
+    assert cmp.check_out('14-HGD-6') == 15.0
 
 # Test al bestaand
 def test_predefined():
     cmp.check_in('BB-49-JF')
     assert cmp.check_in('BB-49-JF') == False
-    cmp.parked_cars = {}
+    cmp.check_out('BB-49-JF')
--- Arch2/Week9/parkingMachineAssignment/carparking.py
+++ Arch2/Week9/parkingMachineAssignment/carparking.py
@@ -7,24 +7,28 @@ class CarParkingMachine:
         self.hourly_rate = hourly_rate
         self.parked_cars = parked_cars
 
-    def check_in(self, lP: str, time: str = datetime.now().strftime("%d:%H:%M")) -> bool or None:
-        if len(self.parked_cars) >= self.capacity or self.parked_cars.get(lP) != None:
+    def check_in(self, license_plate: str, check_in: str = datetime.now()) -> bool or None:
+        if len(self.parked_cars) >= self.capacity or self.parked_cars.get(license_plate) != None:
             return False
         else:
-            self.parked_cars[lP] = ParkedCar(lP, time)
+            self.parked_cars[license_plate] = ParkedCar(license_plate, check_in.strftime("%d:%H:%M"))
 
-    def get_parking_fee(self, lP: str) -> float:
-        time = self.parked_cars.get(lP).checked_in.split(':')
+    def get_parking_fee(self, license_plate: str) -> float:
+        time = self.parked_cars.get(license_plate).checked_in.split(':')
         time = [int(time[0]), int(time[1]) if int(time[2]) < 30 else int(time[1])+1]
         time = [(datetime.now().hour - time[1]+1) 
                     if time[0] == datetime.now().day else 
                         (datetime.now().hour - time[1]+25)][0]
-        self.parked_cars.pop(lP)
         return [self.hourly_rate if time < 1 else time*self.hourly_rate if time <= 24 else 24*self.hourly_rate][0]
 
+    def check_out(self, license_plate: str) -> None:
+        fee = self.get_parking_fee(license_plate)
+        self.parked_cars.pop(license_plate)
+        return fee
+
 class ParkedCar:
-    def __init__(self, lP: str, time) -> None:
-        self.license_plate = lP
+    def __init__(self, license_plate: str, time) -> None:
+        self.license_plate = license_plate
         self.checked_in = time
 
 
@@ -45,7 +49,7 @@ if __name__ == "__main__":
                 print('Capacity reached!')
         elif inp == 'O':
             inp = input().upper()
-            outp = cpm.get_parking_fee(inp)
+            outp = cpm.check_out(inp)
             if outp is not None:
                 print(f'Parking fee: {outp:.2f} EUR')
             else:
