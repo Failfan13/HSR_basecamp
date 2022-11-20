e0aa4f670567abdad7351d438b52f8ec3f6428ff
Erik M <failfan13gta5@gmail.com>
2022-11-20 21:12:28 +0100

    changes longestId

--- Arch3/Week10/longestIdentifier/file.py
+++ Arch3/Week10/longestIdentifier/file.py
@@ -1,19 +1,13 @@
-import sys
-import os
-import re
-
-def main():
-    with open(os.path.join(sys.path[0], 'randomtext.txt'), 'rt') as text:
-        lines = text.readlines()
-        text.close()
-        longestWord = []
-        for line in lines:
-            line = re.compile('[,*\.!?]').sub('', line)
-            longestWord.append(max(line.split(), key=lambda x: (len(x), x), default=''))
-        print('The longest word is: ' + str(len(max(longestWord, key=len))))
-        [print(i) for i in longestWord if len(i) == len(max(longestWord, key=len))]
-        
+def main(file='randomtext.txt'):
+    try:
+        with open(file, 'rt') as text:
+            longestWord = []
+            for line in text:
+                longestWord.append(max(line.split(), key=lambda x: (len(x), x), default=''))
+            print(' '.join([i for i in longestWord if len(i) == len(max(longestWord, key=len))]))
+    except (IOError, OSError):
+        print('invalid')
 
 
 if __name__ == "__main__":
-    main()
+    main(file=input())
