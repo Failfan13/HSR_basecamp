6f8884c4a00d12b43b76565a3e7fa202d88017ef
Erik M <failfan13gta5@gmail.com>
2022-11-20 15:15:18 +0100

    changes carParkingLogger

--- Arch3/Week10/carParkingLoggerAssignment/carparking.py
+++ Arch3/Week10/carParkingLoggerAssignment/carparking.py
@@ -10,7 +10,6 @@ class CarParkingLogger:
         self.capacity = capacity
         self.parked = 0
         self.dateTime = dt.now()
-        self.dateTime.strftime('%d-%m-%Y %H:%M:%S')
         self.dataStr = ''
         self.load_file()
 
@@ -29,18 +28,19 @@ class CarParkingLogger:
                 self.dataStr += (line if line.find('\n') > 0 else line +'\n')
 
     def check_in(self, license_plate: str) -> None:
-        txt = f'{self.dateTime};cpm_name={self.name};license_plate={license_plate};action=check-in'
-        (txt if len(self.dataStr) > 1 else '\r' + txt)
-        if self.dataStr.count(license_plate) % 2 == 0 and self.parked <= self.capacity:
+        dateTime = self.dateTime.strftime('%d-%m-%Y %H:%M:%S')
+        txt = f'{dateTime};cpm_name={self.name};license_plate={license_plate};action=check-in\n'
+        #txt = txt if len(self.dataStr) < 1 else '\r' + txt
+        if self.dataStr.count(license_plate) % 2 == 0 and self.parked < self.capacity:
             with open(os.path.join(sys.path[0], 'carparklog.txt'), 'a') as parkFile:
                 parkFile.write(txt)
             self.dataStr += txt
             self.parked += 1
 
     def check_out(self, license_plate: str) -> bool or None:
-        txt = f'\r{self.dateTime};cpm_name={self.name};license_plate={license_plate};action=check-out'
-        if self.dataStr.count(license_plate) % 2 != 0 and self.parked < self.capacity:
-            txt += ';parking_fee=' + str(self.get_total_car_fee(license_plate))
+        txt = f'{self.dateTime};cpm_name={self.name};license_plate={license_plate};action=check-out'
+        if self.dataStr.count(license_plate) % 2 != 0:
+            txt += ';parking_fee=' + str(self.get_total_car_fee(license_plate)) + '\n'
             with open(os.path.join(sys.path[0], 'carparklog.txt'), 'a') as parkFile:
                 parkFile.write(txt)
             self.dataStr += txt
@@ -48,11 +48,11 @@ class CarParkingLogger:
             return True
             
     def get_total_car_fee(self, license_plate: str) -> float:
-        for line in self.dataStr.split('\n'):
-            if ((re.split('=|;', line)[4] if len(re.split('=|;', line)) > 1 else None) == license_plate):
-                time = -(dt.strptime((line[:19] if line[:1] != '\r' else line[1:20]), '%d-%m-%Y %H:%M:%S') - 
-                            dt.strptime(self.dateTime, '%d-%m-%Y %H:%M:%S')).total_seconds()//3600
-        return self.rate * (24 if time > 24 else time if time > 1 else 1)
+        for line in re.split('\n|\r', self.dataStr):
+            if (re.split('=|;', line)[4] if len(re.split('=|;', line)) > 1 else None) == license_plate:
+                time = -((dt.strptime((line[:19] if line[:1] != '\r' else line[1:20]), '%d-%m-%Y %H:%M:%S')) -
+                        dt.now()).total_seconds() // 3600
+                return self.rate * (24 if time > 24 else time if time > 1 else 1)
 
     def get_machine_fee_by_day(self, cpm_name: str = 'north', date: str = '09-02-2022') -> float:
         fees = 0.00
@@ -65,8 +65,20 @@ class CarParkingLogger:
         return fees
             
 
+def main():
+    cpl = CarParkingMachine(id="North",capacity=2, hourly_rate=4.0)
+    while True:
+        inp = input('''[I] Check-in car by license plate
+[O] Check-out car by license plate
+[Q] Quit program''').lower()
+        if inp == 'i':
+            inp = input('numberplate').upper()
+            cpl.check_in(inp)
+        elif inp == 'o':
+            inp = input('numberplate').upper()
+            cpl.check_out(inp)
+        elif inp == 'q':
+            break
+
 if __name__ == "__main__":
-    ...
-
-
-# round returned total car fee to 2 decimals , # round return machine by day to 2 decimals
+    main()
