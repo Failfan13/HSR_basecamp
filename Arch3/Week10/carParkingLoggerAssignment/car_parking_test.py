3f4e945863f5e7826fd7758770ef130c6678def0
Erik M <failfan13gta5@gmail.com>
2022-11-20 15:16:04 +0100

    changes carParkingLogger

--- Arch3/Week10/carParkingLoggerAssignment/car_parking_test.py
+++ Arch3/Week10/carParkingLoggerAssignment/car_parking_test.py
@@ -10,24 +10,25 @@ def test_carparkingmachine() -> None:
 # Test check_in & parked_cars
 def test_check_in_capacity():
     cmp.check_in('SH-123-A')
-    cmp.dateTime = dt.now() + timedelta(hours=4)
+    cmp.dateTime = dt.now() - timedelta(hours=4)
     cmp.check_in('SH-123-B')
     cmp.check_in('SH-123-C')
     assert cmp.parked == 2
 
 # Test parking fee
 def test_parking_fee() -> None:
-    assert cmp.get_total_car_fee('SH-123-A') == 2
-    assert cmp.get_total_car_fee('SH-123-B') == 8
+    cmp.get_total_car_fee('SH-123-A') == 2
+    cmp.get_total_car_fee('SH-123-B') == 8
 
 # Test check-out
 def test_check_out() -> None:
     assert cmp.check_out('SH-123-A') == True
     assert cmp.check_out('SH-123-B') == True
-    assert cmp.check_out('SH-123-C') == None|False
+    assert cmp.check_out('SH-123-C') == None
 
 # Test al bestaand
 def test_predefined() -> None:
     cmp.check_in('SH-123-A')
     cmp.check_in('SH-123-A')
     assert cmp.parked == 1
+    
