6448b554c631f5642d66fd0a334e219b502cfdfe
Erik M <failfan13gta5@gmail.com>
2022-11-20 20:11:54 +0100

    changes unixCode

--- Arch3/Week10/unixCode/file.py
+++ Arch3/Week10/unixCode/file.py
@@ -1,20 +1,14 @@
-# 1.	Unix-based operating systems usually include a tool named head. It displays the first 10 lines of a file whose name is provided as a command line parameter. 
-# Write a Python program that provides the same behavior. Display an appropriate error message if the file requested by the user does not exist or if the command line parameter is omitted.
 import sys
-import os
 
-def main():
-    with open(os.path.join(sys.path[0], 'randomtext.txt'), 'rt') as ft:
-        ft = ft.readlines()
-    #try: 
-    #    ft = open(os.path.join(sys.path[0], 'randomtext.txt'), newline='', encoding='utf8').readlines()
-    #except (IOError, OSError):
-    #    print('invalid')
-        count = 0
-        while (count <= 10):
-            if not ft[count] in ('\r', '\n', '\r\n'):
-                print(ft[count])
-            count+=1
+
+def main(file=sys.argv[1]):
+    try:
+        with open(file, 'rt') as fileText:
+            fileText = [line for line in fileText if line != '\n']
+            print(''.join([l for i, l in enumerate(fileText) if i <= 10]))
+    except (OSError, IOError):
+        print('invalid')
+
 
 if __name__ == "__main__":
     main()
