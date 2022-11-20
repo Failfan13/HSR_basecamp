0a157069ac805ed44c23ebec0c8268ba87533a77
Erik M <failfan13gta5@gmail.com>
2022-11-20 20:36:17 +0100

    changes unicCodeTail

--- Arch3/Week10/unixCodeTail/file.py
+++ Arch3/Week10/unixCodeTail/file.py
@@ -1,28 +1,19 @@
 import sys
-import os
-import time
 
-def main():
-    ft = open(os.path.join(sys.path[0], 'randomtext.txt'), newline='', encoding='utf-8').readlines()
-    #while '\r\n' in ft:
-        #ft.remove('\r\n')
-    '''
-    start1 = time.time()
-    for line in ft[-10:]:
-        print(line)
-    end1 = time.time()
-    print(end1 - start1)
-    '''
-    '''
-    start2 = time.time()
-    count = 0
-    for line in ft:
-        if count >= len(ft)-10:
-            print(line)
-        count +=1
-    end2 = time.time()
-    print(end2 - start2)
-    '''
+
+def main(file=sys.argv[1]):
+    try:
+        with open(file, 'rt') as fileText:
+            [print(line) for line in fileText.readlines()[-10:]]
+
+            for index, line in enumerate(fileText):
+                if index > len(fileText.readlines()) - 10:
+                    print(line)
+
+            print(''.join([line for line in fileText.readlines()[-10:]]))
+
+    except (IOError, OSError):
+        print('invalid')
 
 
 if __name__ == "__main__":
