f079df3c6ffeb82ad01956b916b8f0fa7eed91c0
Erik M <failfan13gta5@gmail.com>
2022-11-20 04:20:57 +0100

    changes carParkingLogger

--- Arch3/Week10/carParkingLoggerAssignment/carparking.py
+++ Arch3/Week10/carParkingLoggerAssignment/carparking.py
@@ -1,83 +1,57 @@
 import sys
 import os
-from datetime import datetime
+from datetime import datetime as dt
 import re
 
 class CarParkingLogger:
-    def __init__(self, cpm_name: str = 'Undefined', hourly_rate: float = 2.5, dateTime: str = 'ok'):
+    def __init__(self, cpm_name: str = '', hourly_rate: float = 2.5):
         self.name = cpm_name
         self.rate = hourly_rate
-        self.dateTime = dateTime
-        self.dataStr = self.load_file()
+        self.dateTime = dt.now().strftime('%d-%m-%Y %H:%M:%S')
+        self.dataStr = ''
+        self.load_file()
 
-    def load_file(self):
-        return 'oksok'
+    def load_file(self) -> None:
+        parkFile = open(os.path.join(sys.path[0], 'carparklog.txt'), 'rt').readlines()
+        allParked = parkFile
+        allParked.reverse()
+        for line in allParked:
+            if (re.split('=|;', line)[2] == self.name and 
+                    re.split('=|;', line)[6] in ('check-in\n', 'check-in') and
+                        ''.join(allParked).count(re.split('=|;', line)[4]) % 2 != 0 and
+                            ''.join(self.dataStr).count(re.split('=|;', line)[4]) < 1):
+                self.dataStr += (line if line.find('\n') > 0 else line +'\n')
 
-    def check_in(self, license_plate: str):
-        print(self.dataStr)
+    def check_in(self, license_plate: str) -> None:
+        txt = f'\r{self.dateTime};cpm_name={self.name};license_plate={license_plate};action=check-in'
+        if self.dataStr.count(license_plate) % 2 == 0:
+            with open(os.path.join(sys.path[0], 'carparklog.txt'), 'a') as parkFile:
+                parkFile.write(txt)
+            self.dataStr += txt
 
-    def check_out(self, license_plate: str):
-        ...
+    def check_out(self, license_plate: str) -> None:
+        txt = f'\r{self.dateTime};cpm_name={self.name};license_plate={license_plate};action=check-out'
+        if self.dataStr.count(license_plate) % 2 != 0:
+            txt += ';parking_fee=' + str(self.get_total_car_fee(license_plate))
+            with open(os.path.join(sys.path[0], 'carparklog.txt'), 'a') as parkFile:
+                parkFile.write(txt)
+            self.dataStr += txt
+            
 
-    def parking_fee(self, license_plate: str, dateTime: str = 'dateTime'):
-        ...
-
-if __name__ == "__main__":
-    parkingMachine = CarParkingLogger(cpm_name='pepega')
-    parkingMachine.check_in('sb-123-a')
-    parkingMachine.check_in('sb-123-b')
-    parkingMachine.check_in('sb-123-c')
-    parkingMachine.check_out('sb-123-b')
-
-
-#Load text file once load info into string = only checkins if not checkedout
-#Load all checkins that have not been checkedout for specific self.name
-
-#If checkin already exists in checkin list return false
-    # date' 'time;cpm_name=...;license_plate=...;action=check-in
-
-#If checkout license plate is in checkin list else return false
-    # date' 'time;cpm_name=...;license_plate=...;action=check-in
-
-#parking fee based on license plate in checkin data has own date time argument
-
-
-
-
-'''
-print(datetime.now())
-
-class CarParkingLogger:
-    def __init__(self, cpm_name: str = 'Undefined', hourly_rate: float = 2.5, dateTime: str = 'ok') -> None:
-        self.name = cpm_name
-        self.rate = hourly_rate
-        self.dateTime = dateTime
-        self.file = os.path.join(sys.path[0], 'carparklog.txt')
-
-    def check_in(self, license_plate) -> None:
-        text = f'{self.dateTime};cpm_name={self.name};license_plate={license_plate};action=check-in\n'
-        open(self.file, 'a').writelines(text)
-
-    def check_out(self, license_plate) -> None:
-        license_plates = [re.split('=|;', i)[4] for i in open(self.file, 'r').readlines()]
-        if license_plate in license_plates and license_plates.count(license_plate)%2 != 0:
-            text = f'{self.dateTime};cpm_name={self.name};license_plate={license_plate};action=check-out;parking_fee='
-            open(self.file, 'a').writelines(text + self.get_total_car_fee(license_plate) + '\n')
-        else:
-            return False
-
-    def get_total_car_fee(self, lisence_plate) -> int or float:
-        return 'bruh'
-
+    def get_total_car_fee(self, license_plate: str, dateTime: str = dt.now().strftime('%d-%m-%Y %H:%M:%S')) -> float:
+        for line in self.dataStr.split('\n'):
+            if ((re.split('=|;', line)[4] if len(re.split('=|;', line)) > 1 else None) == license_plate):
+                time = -(dt.strptime(line[:19], '%d-%m-%Y %H:%M:%S') - 
+                            dt.strptime(dateTime, '%d-%m-%Y %H:%M:%S')).total_seconds()//3600
+        return self.rate * (time if time > 1 else 1)
+            
 
 if __name__ == "__main__":
-    parkingMachine = CarParkingLogger(cpm_name='pepega')
-    parkingMachine.check_in('sb-123-a')
-    parkingMachine.check_in('sb-123-b')
-    parkingMachine.check_in('sb-123-c')
-    parkingMachine.check_out('sb-123-b')
+    parkingMachine = CarParkingLogger(cpm_name='North')
+    ...
 
-'''
-# Datetime toevoegen aan checkin
-# Maken manier laatste check-in te krijgen en daar de tijd uit halen
-# If checkout car not in list return
+
+# get_machine_fee_by_day method die 2 argumenten neemt, cpm_name : case insensitive(.title() is handig), search date form: DD-MM-YYYY,
+# the returned fee should be rounded to 2 decimals
+
+# round returned total car fee to 2 decimals
