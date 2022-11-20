f6995f30f66bc294fa92b1220f6fce93555145f2
Erik M <failfan13gta5@gmail.com>
2022-11-21 00:30:54 +0100

    changes removeComments

--- Arch3/Week10/removeComments/file.py
+++ Arch3/Week10/removeComments/file.py
@@ -1,17 +1,17 @@
 def main(fileName: str) -> None:
-    with open(fileName, 'rt') as fileText:
-        text = []
-        for index, line in enumerate(fileText):
-            if ((line.split()[0] if len(line.split()) != 0 else '') == 'def' and 
-                    (text[index - 1].find('#') if index != 0 else True)):
-                print(f'''File: {fileName} contains a function [{line.split()[1][:-1]}] 
+    for file in fileName.split(', '):
+        try:
+            with open(file, 'rt') as fileText:
+                text = []
+                for index, line in enumerate(fileText):
+                    if ((line.split()[0] if len(line.split()) != 0 else '') == 'def'):
+                        if (text[index - 1].find('#') if index != 0 else True):
+                            print(f'''File: {file} contains a function [{line.split()[1][:-1]}]
 on line [{index + 1}] without a preceding comment.''')
-            text.append(line)
-
-# split input ', ' for loop each file.
-
-# try catch error IO OS
+                    text.append(line)
+        except (IOError, OSError):
+            print('invalid')
 
 
 if __name__ == "__main__":
-    main(fileName='randomtext.txt')
+    main(fileName=input())
