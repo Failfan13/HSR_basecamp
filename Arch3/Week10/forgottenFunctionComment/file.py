cf45d19105ad322962a5ae04d6456aaa61ac0572
Erik M <failfan13gta5@gmail.com>
2022-11-21 14:06:53 +0100

    changes forgottenFunction

--- Arch3/Week10/forgottenFunctionComment/file.py
+++ Arch3/Week10/forgottenFunctionComment/file.py
@@ -1,9 +1,10 @@
 def check_comment(lines: dict, fileName: str) -> str:
     textFile = ''
     for key, val in lines.items():
-        if 'def' in val.split() and (lines[key - 1].find('#') if key != 1 else key == 1):
-            textFile += '# todo: add comment to this function \n'
-        textFile += val
+        if val.find('#') >= 0:
+            textFile += ''
+        else:
+            textFile += val
     return textFile
 
 
@@ -11,11 +12,11 @@ def main(newFile='testfile.txt', file='python1.py'):
     try:
         with open(file, 'rt') as pyFile:
             dictLine = {k + 1: v for k, v in enumerate(pyFile)}
-        with open(newFile, 'w') as newPyFile:
+        with open(newFile, 'wt') as newPyFile:
             newPyFile.writelines(check_comment(dictLine, pyFile.name))
     except (OSError, IOError):
         print('invalid')
 
 
 if __name__ == "__main__":
-    main(newFile=input(), file=input())
+    main(file=input(), newFile=input())
