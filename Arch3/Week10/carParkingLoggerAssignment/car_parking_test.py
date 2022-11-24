2eef2aa4b668e1718fb12477446002981d4f8f35
Erik M <failfan13gta5@gmail.com>
2022-11-24 14:48:06 +0100

    changes carParkingLogger

--- Arch3/Week10/carParkingLoggerAssignment/car_parking_test.py
+++ Arch3/Week10/carParkingLoggerAssignment/car_parking_test.py
@@ -35,6 +35,7 @@ def test_predefined():
     assert False == cmp.check_in('BB-49-JF')
     cmp.check_out('BB-49-JF')
 
+# Test alle kosten per datum berekenen
 def test_get_machine_fee_by_day():
     cpm = CarParkingMachine(id='0123796423643512323', hourly_rate=1.55)
     
@@ -48,5 +49,40 @@ def test_get_machine_fee_by_day():
     cpm.check_out(license_plate='LLL')
     cpm.check_in(license_plate='MMM')
 
-    cpm.logger.get_machine_fee_by_day('0123796423643512323', dt.now().strftime('%d-%m-%Y'))
-test_get_machine_fee_by_day()
+    assert 41.85 == cpm.logger.get_machine_fee_by_day('0123796423643512323', dt.now().strftime('%d-%m-%Y'))
+
+# Test kosten van bepaalde nummerplaat berekenen
+def test_get_total_car_fee():
+    cpm_1 = CarParkingMachine(id='cpm_1', hourly_rate=4)
+    cpm_1.check_in('123-ab-4', check_in=dt.now() - timedelta(hours=30))
+    cpm_1.check_out('123-ab-4')
+    cpm_1.check_in('123-ab-4')
+    cpm_1.check_out('123-ab-4')
+
+    cpm_2 = CarParkingMachine(id='cpm_2', hourly_rate=0.5)
+    cpm_2.check_in('123-ab-4')
+    cpm_2.check_out('123-ab-4')
+
+    cpm_3 = CarParkingMachine(id='cpm_3', hourly_rate=6.25)
+    cpm_3.check_in('123-ab-4')
+    cpm_3.check_out('123-ab-4')
+    cpm_3.check_in('123-ab-4')
+    
+    assert 100 == cpm_1.logger.get_total_car_fee('123-ab-4')
+    assert 0.5 == cpm_2.logger.get_total_car_fee('123-ab-4')
+    assert 6.25 == cpm_3.logger.get_total_car_fee('123-ab-4')
+
+
+def test_restore_state():
+        check_in = datetime.now() - timedelta(minutes=10)
+    
+        cpm_north = CarParkingMachine(id='cpm_1')
+        cpm_north.check_in(license_plate='RRR', check_in=check_in)
+        cpm_north.check_in(license_plate='TTT', check_in=check_in)
+        cpm_north.check_out(license_plate='TTT')
+
+        cpm_north = CarParkingMachine(id='cpm_1')
+        assert True == ('RRR' in cpm_north.parked_cars)
+        assert True == check_in.replace(microsecond=0) == cpm_north.parked_cars['RRR'].check_in
+
+test_restore_state()
--- Arch3/Week10/carParkingLoggerAssignment/carparking.py
+++ Arch3/Week10/carParkingLoggerAssignment/carparking.py
@@ -9,10 +9,10 @@ class CarParkingMachine:
         self.parked_cars.update(self.logger.check_state())
 
 
-    def check_in(self, license_plate: str, check_in: str = dt.now()) -> None:
+    def check_in(self, license_plate: str, check_in = dt.now()) -> None:
         if (license_plate not in self.parked_cars.keys() and len(self.parked_cars) < self.capacity):
             if self.parked_cars.get(license_plate) != bool or None:
-                self.parked_cars[license_plate] = ParkedCar(license_plate, check_in.strftime('%d-%m-%Y %H:%M:%S'))
+                self.parked_cars[license_plate] = ParkedCar(license_plate, check_in.replace(microsecond=0))
                 self.logger.check_in(self.parked_cars[license_plate])
         else:
             return False
@@ -20,7 +20,7 @@ class CarParkingMachine:
 
     def get_parking_fee(self, license_plate: str) -> float:
         if license_plate in self.parked_cars.keys():
-            time = - (dt.strptime(self.parked_cars.get(license_plate).checked_in, '%d-%m-%Y %H:%M:%S') -
+            time = - (self.parked_cars.get(license_plate).check_in -
                         dt.now()).total_seconds() //3600
             return self.hourly_rate * (24 if time > 24 else time if time > 1 else 1)
 
@@ -37,24 +37,28 @@ class CarParkingMachine:
 class ParkedCar:
     def __init__(self, license_plate: str, time) -> None:
         self.license_plate = license_plate
-        self.checked_in = time
+        self.check_in = dt.strptime(str(time), "%Y-%m-%d %H:%M:%S")
+
+
 
 class CarParkingLogger:
     def __init__ (self, cpm_name):
         self.cpm_name = cpm_name
-        self.writeFile = open('carparklog.txt', 'a')
 
     def read_file(self):
         with open('carparklog.txt', 'r') as f:
             return f.readlines()
 
+    def write_file(self, inp):
+        with open('carparklog.txt', 'a') as w:
+            return w.write(inp)
+
     def check_in(self, *args):
-            self.writeFile.write(f'{args[0].checked_in};cpm_name={self.cpm_name};license_plate={args[0].license_plate};action=check-in\n')
+        self.write_file(f'{args[0].check_in};cpm_name={self.cpm_name};license_plate={args[0].license_plate};action=check-in\n')
 
 
     def check_out(self, *args):
-            self.writeFile.write(
-                f'{args[0].checked_in};cpm_name={self.cpm_name};license_plate={args[0].license_plate};action=check-out;parking_fee={args[1]}\n')
+        self.write_file(f'{args[0].check_in};cpm_name={self.cpm_name};license_plate={args[0].license_plate};action=check-out;parking_fee={args[1]}\n')
 
     def check_state(self) -> dict:
         checkedIn = {}
@@ -64,26 +68,23 @@ class CarParkingLogger:
             plate = line.split(';')[2][line.split(';')[2].index('=') + 1:]
             if ''.join(text).count(plate) % 2 != 0:
                 if plate not in checkedIn.keys():
-                    checkedIn[plate] = ParkedCar(plate, line[:19])
+                    checkedIn[plate] = ParkedCar(plate, str(line[:19]))
         return checkedIn
 
     def get_machine_fee_by_day(self, *args) -> float:
         fee = 0
-        print(self.read_file())
-        #for line in self.read_file():
-        #    print(line)
-        #    if (line.split(';')[1] == f'cpm_name={args[0]}' and len(line.split(';')) > 4):
-        #        fee += float(line.split(';')[4][line.split(';')[4].index('=') + 1:])
-        return fee
+        for line in self.read_file():
+            if (line.split(';')[1] == f'cpm_name={args[0]}' and len(line.split(';')) > 4):
+                fee += float(line.split(';')[4][line.split(';')[4].index('=') + 1:])
+        return round(fee, 2)
     
     
     def get_total_car_fee(self, license_plate):
         fee = 0
-
         for line in self.read_file():
-            if line.find(license_plate) >= 0 and len(line.split(';')) > 4:
+            if line.find(license_plate) >= 0 and len(line.split(';')) > 4 and line.find(self.cpm_name) >= 0:
                 fee += float(line.split(';')[4][line.split(';')[4].index('=') + 1:])
-        return fee
+        return round(fee, 2)
 
 
 if __name__ == "__main__":
