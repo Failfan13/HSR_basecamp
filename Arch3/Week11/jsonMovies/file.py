0419cd790f5c399eeb6aa08b0d772f2b35bb45c1
Erik M <failfan13gta5@gmail.com>
2022-11-28 00:26:52 +0100

    changes jsonMovies

--- Arch3/Week11/jsonMovies/file.py
+++ Arch3/Week11/jsonMovies/file.py
@@ -1,13 +1,16 @@
+# Defenition
 import sys
 import os
 import json
 
+# Processing
 class Movies:
+    # initialisation
     def __init__(self, file: str = 'movies.json') -> None:
         self.file = file
         self.list = readJson(self.file)
 
-
+    # Shows all requested info by exercise
     def showInfo(self) -> None:
         line = ''
         movie2004 = 0
@@ -15,7 +18,6 @@ class Movies:
         movieKeau = ''
         movieSylve = ''
         for dict in self.list:
-            #text = f"(title= {dict.get('title')}, year= {dict.get('year')}, cast= {dict.get('cast')}, genres= {dict.get('genres')})"
             text = f"(title: {dict.get('title')})"
             line += text
             if dict.get('year') == 2004:
@@ -26,10 +28,9 @@ class Movies:
                 movieKeau += text
             if str(dict.get('cast')).find('Sylvester') >= 0 and int(dict.get('year')) in range(1995, 2006):
                 movieSylve += text
-                
         print(movie2004, movieScience, movieKeau, movieSylve)
 
-
+    # Modifies data requested by exercise
     def modification(self) -> None:
         lowest = min(self.list, key=lambda x: x.get('year')).get('year')
         for i, dict in enumerate(self.list):
@@ -38,13 +39,13 @@ class Movies:
             if dict.get('year') == lowest:
                 self.list[i]['year'] = self.list[i]['year'] - 1
             if str(dict.get('cast')).find('Natalie Portman') >= 0:
-                self.list[i]['cast'] = list(map(lambda x: x.replace('Natalie Portman', 'Nat Portman'), self.list[i]['cast']))
+                self.list[i]['cast'] = list(map(
+                    lambda x: x.replace('Natalie Portman', 'Nat Portman'), self.list[i]['cast']))
             if str(dict.get('cast')).find('Kevin Spacey') >= 0:
                 self.list[i]['cast'].remove('Kevin Spacey')
-        
         writeJson(self.file, self.list)
 
-    
+    # Changes existing name and year
     def change(self, inp) -> None:
         nameInp = input('New movie name')
         dateInp = input('New release year')
@@ -54,26 +55,25 @@ class Movies:
                     self.list[i]['title'] = nameInp.title()
                 if not dateInp.replace(' ', '') == '':
                     self.list[i]['year'] = int(dateInp)
-        
         writeJson(self.file, self.list)
-
-
+    
+    # Search in self.lines (json text)
     def search(self, inp) -> dict:
         for dict in self.list:
             if inp in dict.values():
                 return dict
 
-
+# Reads from json file
 def readJson(file) -> list:
-    with open(os.path.join(sys.path[0], file), mode='r',encoding='UTF-8') as file:
+    with open(os.path.join(sys.path[0], file), mode='r', encoding='UTF-8') as file:
         return json.load(file)
 
-
+# Writes to json file
 def writeJson(file, text) -> None:
     with open(os.path.join(sys.path[0], file), mode='w') as file:
         json.dump(text, file, indent=4)
 
-
+# Main method / menu
 def main() -> None:
     MList = Movies()
     while True:
@@ -99,6 +99,6 @@ def main() -> None:
         else:
             print('Invalid')
 
-
+# Method calling
 if __name__ == '__main__':
     main()
