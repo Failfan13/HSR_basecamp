51fc9d693b645211afac38541d720d828f6aab33
Erik M <failfan13gta5@gmail.com>
2022-12-11 13:38:15 +0100

    changes organiseStudents

--- Arch4/organiseStudent/file.py
+++ Arch4/organiseStudent/file.py
@@ -1,23 +1,75 @@
 import sys
 import os
-import json
-import math
 import sqlite3 as sql
 
 class studentOrganiser():
-    def __init__(self):
+    def __init__(self, sqlCon:sql.Connection):
         self.dataBaseInf = 0
+        self.sqlCon = sqlCon
 
     # Function for creating student -> student
-    def newStudent():
-        ...
+    def newStudent(self) -> int:
+        stuData = dictQuestions(['first_name', 'last_name', 'city', 'date_of_birth', 'class'])
+        self.sqlCon.execute(
+            f'''INSERT INTO students {tuple(stuData.keys())}
+                VALUES {tuple(stuData.values())}
+            '''
+        )
+        return next(self.sqlCon.execute(
+            f'''SELECT studentnumber
+                FROM students 
+                WHERE first_name = "{stuData.get('first_name')}" 
+                AND last_name = "{stuData.get('last_name')}"
+                AND date_of_birth = "{stuData.get('date_of_birth')}"
+                AND city = "{stuData.get('city')}"
+                '''
+        ))[0]
 
-    def searchAssignStudent():
-        ...
+    def assignStudent(self) -> None:
+        stuData = dictQuestions(['studentnumber', 'class'])
+        cur = self.sqlCon.cursor()
+        cur.execute(
+            f'''SELECT studentnumber from students where studentnumber = "{stuData.get('studentnumber')}"'''
+            )
+        if cur.fetchall():
+            cur.execute(
+                f'''UPDATE students
+                    SET class = "{stuData.get('class')}"
+                    WHERE studentnumber = "{stuData.get('studentnumber')}"'''
+            )
+        else:
+            print(f"invalid student: {stuData.get('studentnumber')}")
 
-    # List all students > no class given = all -> class descOrd, with class is in that classroom -> stNr asOrd 
-    def listStudents(classRoom='all'):
-        ...
+    def listStudents(self, classRoom='all') -> None:
+        if classRoom == 'all':
+            stuData = {'class': 'TRUE', 'order': 'class DESC'}
+        else:
+            stuData = dictQuestions(["class"])
+            stuData['class'] = 'class = "{}"'.format(stuData.get('class'))
+            stuData['order'] = 'studentnumber ASC'
+        cur = self.sqlCon.cursor()
+        cur.execute(
+            f'''SELECT * FROM students
+                WHERE {stuData.get('class')}
+                ORDER BY {stuData.get('order')}'''
+        )
+        print(cur.fetchall())
+
+    def searchStudent(self) -> None:
+        stuData = dictQuestions(['first_name', 'last_name_or_city'])
+        cur = self.sqlCon.cursor()
+        cur.execute(
+            f'''SELECT * FROM students
+                WHERE first_name = "{stuData.get('first_name')}" 
+                    AND last_name = "{stuData.get('last_name_or_city')}" 
+                    OR city = "{stuData.get('last_name_or_city')}"'''
+        )
+        print(cur.fetchone())
+
+
+def dictQuestions(questions:list) -> dict:
+    return {q:input(q).title() for q in questions}
+
 
 def menuStructure():
     """[A] Add new student
@@ -40,24 +92,25 @@ def main():
             class TEXT DEFAULT NULL 
         );'''
     )
-    # while True:
-    #     inp = input(menuStructure.__doc__).upper()
-    #     if inp == 'A':
-    #         ...
-    #     elif inp == 'C':
-    #         ...
-    #     elif inp == 'D':
-    #         ...
-    #     elif inp == 'L':
-    #         ...
-    #     elif inp == 'S':
-    #         ...
-    #     elif inp == 'Q':
-    #         ...
-    #     else:
-    #         print('invalid')
-    #     break
+    stuOrg = studentOrganiser(connect)
+    while True:
+        inp = input(menuStructure.__doc__).upper()
+        if inp == 'A':
+            stuOrg.newStudent()
+        elif inp == 'C':
+            stuOrg.assignStudent()
+        elif inp == 'D':
+            stuOrg.listStudents()
+        elif inp == 'L':
+            stuOrg.listStudents('other')
+        elif inp == 'S':
+            stuOrg.searchStudent()
+        elif inp == 'Q':
+            break
+        else:
+            print('invalid')
+        connect.commit()
 
 
 if __name__ == "__main__":
-    print(main().__doc__)
+    main()
